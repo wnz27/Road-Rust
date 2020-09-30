872f79e55299e3f3f7b795ccc0057a3a581a87d8
fzk27 <feng@littlelights.ai>
2020-09-30 18:55:17 +0800

    update little demo

---
+++ rust-pro/guess_game/src/main.rs
@@ -0,0 +1,37 @@
+use rand::Rng;
+use std::io;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+    
+    let secret_num = rand::thread_rng().gen_range(1, 101);
+    
+    loop {
+        println!("Please input your guess number!");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!!!"),
+            Ordering::Greater => println!("Too big!!!"),
+            Ordering::Equal => {
+                println!("You win!!!");
+                break;
+            }
+        }
+    }
+
+
+}
--- rust-pro/rust-pro-category.md
+++ rust-pro/rust-pro-category.md
@@ -1,11 +1,12 @@
 <!--
  * @Date: 2020-09-27 12:34:47
- * @LastEditTime: 2020-09-27 15:40:55
+ * @LastEditTime: 2020-09-30 17:59:40
 -->
 
 # rust-pro-category
 
 - [hellow-world](./hellow_world/src/main.rs)
 - [little_app_demo](./little_app_demo/src/main.rs)
+- [guess-game](./guess_game/src/main.rs)
 
 [【返回上一级】](../README.md)
